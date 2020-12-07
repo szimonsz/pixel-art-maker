e72e8996ad9be34d02cf7690e40481b03a7afea1
szimonsz <70579613+szimonsz@users.noreply.github.com>
2020-12-07 08:04:15 +0000

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,32 +1,32 @@
-let color = document.getElementById('pixelCanvas');
-let log = document.getElementById('log');
+const color = document.getElementById('pixelCanvas');
+const log = document.getElementById('log');
 
 sizePicker.addEventListener('submit', makeGrid);
 
 function makeGrid(event) {
-  let gridH = Number(document.getElementById('inputHeight').value);
-  let gridW = Number(document.getElementById('inputWidth').value);
+    let gridH = Number(document.getElementById('inputHeight').value);
+    let gridW = Number(document.getElementById('inputWidth').value);
 
-  event.preventDefault();
+    event.preventDefault();
 
-  function addTable() {
-    let t = document.getElementById('pixelCanvas');
-    while(t.hasChildNodes())
-       t.removeChild(t.firstChild);
+    function addTable() {
+        let t = document.getElementById('pixelCanvas');
+        while (t.hasChildNodes())
+            t.removeChild(t.firstChild);
 
-    for (let i = 0; i < gridH; i++) {
-      let newTR = t.insertRow(i);
-      for (let j = 0; j < gridW; j++ ){
-        let newTD = newTR.insertCell(j);
-      color.addEventListener('click', addColor)
-      }
-    };
-  }
-addTable();
+        for (let i = 0; i < gridH; i++) {
+            let newTR = t.insertRow(i);
+            for (let j = 0; j < gridW; j++) {
+                let newTD = newTR.insertCell(j);
+                color.addEventListener('click', addColor)
+            }
+        };
+    }
+    addTable();
 }
 
 function addColor(event) {
-  event.target.style.backgroundColor = colorPicker.value;
+    event.target.style.backgroundColor = colorPicker.value;
 }
 
 //In order to be able to create this project I have used the following websites:
